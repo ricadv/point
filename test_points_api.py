24bf07c25b051e83e31a2c33bb139b159eae949e
rica de vera <ricadevera@live.com>
2021-10-13 16:56:17 +0000

    Add test_points_api.py

---
+++ test_points_api.py
@@ -0,0 +1,41 @@
+import unittest
+import points_api
+import xmlrunner
+import os
+import inspect
+from point_manager import PointManager
+from sqlalchemy import create_engine
+from base import base
+
+class TestPointApi(unittest.TestCase):
+
+    def setUp(self):
+        engine = create_engine('sqlite:///test_points.sqlite')
+
+        Base.metadata.create_all(engine)
+        Base.metadata.bind = engine
+
+        points_api.point_mgr = PointManager('test_points.sqlite')
+        
+        points_api.app.testing = True
+        self.app = points_api.app.test_client()
+
+        self.logPoint()
+    
+    def tearDown(self):
+        os.remove('test_points.sqlite')
+        self.logPoint()
+
+    def logPoint(self):
+        currentTest = self.id().spllit('.')[-1]
+        callingFunction = inspect.stack()[1][3]
+        print('in %s - %s()' % (currentTest, callingFunction))
+    
+    def test_points_all(self):
+        rv = self.app.get('/points/all')
+        self.assertEqual(rv.status, '200 OK')
+
+if __name__ == '__main__':
+    runner = xmlrunner.XMLTestRunner(output='api-test-reports')
+    unittest.main(testRunner=runner)
+    unittest.main()
