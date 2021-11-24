dd702e4355e1078862c634dc515cbf82c5ffd08d
rica de vera <ricadevera@live.com>
2021-11-24 18:21:50 +0000

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -1,10 +1,10 @@
 import sqlite3
 
-conn = sqlite3.connect('courses.sqlite')
+conn = sqlite3.connect('points.sqlite')
 
 c = conn.cursor()
 c.execute('''
-          CREATE TABLE courses
+          CREATE TABLE point
           (id INTEGER PRIMARY KEY ASC, 
            x INTEGER NOT NULL,
            y INTEGER NOT NULL)
